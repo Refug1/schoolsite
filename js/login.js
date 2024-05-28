0e3ebbeb2df913777c27afc5397aa536c155d6dd
Refug1 <kirillezic@gmail.com>
2024-05-29 02:59:44 +0300

    massedit

--- js/login.js
+++ js/login.js
@@ -1,9 +1,25 @@
+export let accessLvl = new Boolean(false);
+
+
+window.addEventListener('load', () => {
+    if(accessLvl === true){
+        document.getElementById("loginId").style.visibility = "hidden";
+        alert("Я скрыл кнопку входа");
+    } else {
+        alert("Пока без авторизации")
+    }
+  });
+
 function sendform() {
     let login = document.getElementById('login');
     let password = document.getElementById('password');
     if(login.value == "123" && password.value == "123"){
-        alert("Авторизация прошла успешно");
-        window.location.href = '../index.html';
+        alert("Авторизация пользователя")
+        window.location.href = './index.html';
+    }else if (login.value == "admin" && password.value == "admin"){
+        accessLvl = true;
+        alert("Авторизация администратором");
+        window.location.href = './index.html';
     }else{
         return false;
     }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "calc(125px + (250 - 125) * ((100vw - 500px) / (1920 - 500)))";
+    document.getElementById("mySidepanel").style.width = "calc(140px + (280 - 140) * ((100vw - 500px) / (1920 - 500)))";
 }
 
 function closeNav() {
