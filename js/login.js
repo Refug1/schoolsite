6aa9455f52f9a756a0adc003f67d75c09b0b8efa
Refug1 <kirillezic@gmail.com>
2024-05-29 18:14:37 +0300

    auth

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
-export let accessLvl = new Boolean(false);
-
-
+/* old
 window.addEventListener('load', () => {
     if(accessLvl === true){
         document.getElementById("loginId").style.visibility = "hidden";
@@ -9,18 +7,18 @@ window.addEventListener('load', () => {
         alert("Пока без авторизации")
     }
   });
-
+*/
 function sendform() {
     let login = document.getElementById('login');
     let password = document.getElementById('password');
-    if(login.value == "123" && password.value == "123"){
+    if (login.value == "123" && password.value == "123") {
         alert("Авторизация пользователя")
-        window.location.href = './index.html';
-    }else if (login.value == "admin" && password.value == "admin"){
-        accessLvl = true;
+        window.location.href = './main.html';
+    } else if (login.value == "admin" && password.value == "admin") {
         alert("Авторизация администратором");
-        window.location.href = './index.html';
-    }else{
+        window.location.href = './bp/index.html';
+    } else {
+        alert("Неверный логин или пароль");
         return false;
     }
 }
